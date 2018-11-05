63f2f6ff5d35f460620f0c0e7403d23f92109be3
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-11-05 00:14:39 -0500

    PizzaOrder Semi Finished

---
+++ PizzaOrder(KuotM)/PizzaOrder(KuotM)/PizzaOrderForm.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaOrder_KuotM_
+{
+    public partial class frmPizzaOrder : Form
+    {
+        const double SMALL = 4.99;
+        const double MEDIUM = 6.99;
+        const double LARGE = 9.99;
+        const double XLARGE = 12.99;
+        const double TOPPING = 0.75;
+        const double ONE_TOPPING = 0.75;
+        const double TWO_TOPPING = 1.50;
+        const double THREE_TOPPING = 2.25;
+        const double DELIVERY = 5;
+        const double HST = 0.13;
+        double PIZZA_SIZE;
+        double NUM_TOPPINGS;
+        double SIDE_DISH;
+        double YES = 3.99;
+        double TAX, TOTAL;
+        double SUBTOTAL;
+  
+       System.Media.SoundPlayer KrustyKrab = new System.Media.SoundPlayer(Properties.Resources.KrustyKrab1);
+       System.Media.SoundPlayer Campfire = new System.Media.SoundPlayer(Properties.Resources.FUN);
+
+        public frmPizzaOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPizzaOrder_Load(object sender, EventArgs e)
+        {
+            picPizza.Hide();
+            lblStateSubTotal.Hide();
+            lblStateTax.Hide();
+            lblDelivery.Hide();
+            lblStateTotal.Hide();
+            lblSubTotal.Hide();
+            lblTax.Hide();
+            lblDeliveryOption.Hide();
+            lblTotal.Hide();   
+        }
+
+        private void radSmall_CheckedChanged(object sender, EventArgs e)
+        {
+            PIZZA_SIZE = SMALL;
+            if (radDelivery.Checked == true)
+            {
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX + DELIVERY;
+
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL, 2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX, 2) + "$");
+                lblDelivery.Text = "Delivery";
+                lblDeliveryOption.Text = Convert.ToString(DELIVERY + "$");
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL, 2) + "$");
+            }
+            else
+            {
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX;
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL, 2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX, 2) + "$");
+                lblDelivery.Text = "Pick up";
+                lblDeliveryOption.Text = "$0.00";
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL, 2) + "$");
+            }
+
+        }
+
+        private void radMedium_CheckedChanged(object sender, EventArgs e)
+        {
+            PIZZA_SIZE = MEDIUM;
+            if (radDelivery.Checked == true)
+            {
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX + DELIVERY;
+
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL, 2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX, 2) + "$");
+                lblDelivery.Text = "Delivery";
+                lblDeliveryOption.Text = Convert.ToString(DELIVERY + "$");
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL, 2) + "$");
+            }
+            else
+            {
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX;
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL, 2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX, 2) + "$");
+                lblDelivery.Text = "Pick up";
+                lblDeliveryOption.Text = "$0.00";
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL, 2) + "$");
+            }
+        }
+
+        private void radLarge_CheckedChanged(object sender, EventArgs e)
+        {
+            PIZZA_SIZE = LARGE;
+            if (radDelivery.Checked == true)
+            {
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX + DELIVERY;
+
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL, 2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX, 2) + "$");
+                lblDelivery.Text = "Delivery";
+                lblDeliveryOption.Text = Convert.ToString(DELIVERY + "$");
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL, 2) + "$");
+            }
+            else
+            {
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX;
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL, 2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX, 2) + "$");
+                lblDelivery.Text = "Pick up";
+                lblDeliveryOption.Text = "$0.00";
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL, 2) + "$");
+            }
+        }
+
+        private void radXLarge_CheckedChanged(object sender, EventArgs e)
+        {
+            PIZZA_SIZE = XLARGE;
+        }
+
+        private void picMusic_Click(object sender, EventArgs e)
+        {
+            KrustyKrab.Stop();
+            Campfire.Stop();
+  
+        }
+
+        private void mniMainTheme_Click(object sender, EventArgs e)
+        {
+            KrustyKrab.Play();
+        }
+
+        private void mniFUN_Click(object sender, EventArgs e)
+        {            
+            Campfire.Play();
+        }
+
+        private void mniEnglish_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniFrançais_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void mniEspañol_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radPickup_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radPickup.Checked == true)
+            {
+                lblStateSubTotal.Show();
+                lblSubTotal.Show();
+                lblStateTax.Show();
+                lblTax.Show();
+                lblDelivery.Show();
+                lblDeliveryOption.Show();
+                lblStateTotal.Show();
+                lblTotal.Show();
+
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX;
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL,2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX,2) + "$");
+                lblDelivery.Text = "Pick up";
+                lblDeliveryOption.Text = "$0.00";
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL,2) + "$");
+            }
+            else
+            {
+                lblStateSubTotal.Hide();
+                lblStateTax.Hide();
+                lblDelivery.Hide();
+                lblStateTotal.Hide();
+                lblSubTotal.Hide();
+                lblTax.Hide();
+                lblDeliveryOption.Hide();
+                lblTotal.Hide();
+            }
+        }
+
+        private void chkPepperoni_CheckedChanged(object sender, EventArgs e)
+        {
+                NUM_TOPPINGS = ONE_TOPPING;
+                picPizza.Image = Properties.Resources.pepperoni_pizza;
+                picPizza.Show();
+            if (chkBacon.Checked == true)
+            {
+                NUM_TOPPINGS = TWO_TOPPING;
+                picPizza.Image = Properties.Resources.pepperoni_bacon_pizza;
+            }
+
+            else if (chkOlives.Checked == true)
+            {
+                NUM_TOPPINGS = TWO_TOPPING;
+                picPizza.Image = Properties.Resources.pepperoni_olives_pizza;
+            }
+            else if (chkPepperoni.Checked == false)
+            {
+                NUM_TOPPINGS = 0;
+                picPizza.Image = Properties.Resources.cheese_pizza;
+            }
+
+        }
+
+        private void chkBacon_CheckedChanged(object sender, EventArgs e)
+        {            
+                NUM_TOPPINGS = ONE_TOPPING;
+                picPizza.Image = Properties.Resources.bacon_pizza;
+                picPizza.Show();
+            if (chkPepperoni.Checked == true)
+            {
+                NUM_TOPPINGS = TWO_TOPPING;
+                picPizza.Image = Properties.Resources.pepperoni_bacon_pizza;
+            }
+            else if (chkOlives.Checked == true)
+            {
+                NUM_TOPPINGS = TWO_TOPPING;
+                picPizza.Image = Properties.Resources.bacon_olives_pizza;
+            }
+            else if (chkBacon.Checked == false)
+            {
+                NUM_TOPPINGS = 0;
+                picPizza.Image = Properties.Resources.cheese_pizza;
+            }
+
+        }
+
+        private void chkOlives_CheckedChanged(object sender, EventArgs e)
+        {    
+                NUM_TOPPINGS = ONE_TOPPING;
+                picPizza.Image = Properties.Resources.olives_pizza;
+                picPizza.Show();
+            if (chkPepperoni.Checked == true)
+            {
+                NUM_TOPPINGS = TWO_TOPPING;
+                picPizza.Image = Properties.Resources.pepperoni_olives_pizza;
+            }
+            else if (chkBacon.Checked == true)
+            {
+                NUM_TOPPINGS = TWO_TOPPING;
+                picPizza.Image = Properties.Resources.bacon_olives_pizza;
+            }
+            else if (chkOlives.Checked == false)
+            {
+                NUM_TOPPINGS = 0;
+                picPizza.Image = Properties.Resources.cheese_pizza;
+            }
+
+        }
+
+        private void radFries_CheckedChanged(object sender, EventArgs e)
+        {
+            SIDE_DISH = YES;           
+        }
+        private void radWings_CheckedChanged(object sender, EventArgs e)
+        {
+            SIDE_DISH = YES;
+        }
+
+        private void radSalad_CheckedChanged(object sender, EventArgs e)
+        {
+            SIDE_DISH = YES;
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radDelivery_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radDelivery.Checked == true)
+            {
+                lblStateSubTotal.Show();
+                lblSubTotal.Show();
+                lblStateTax.Show();
+                lblTax.Show();
+                lblDelivery.Show();
+                lblDeliveryOption.Show();
+                lblStateTotal.Show();
+                lblTotal.Show();
+
+                SUBTOTAL = PIZZA_SIZE + NUM_TOPPINGS + SIDE_DISH;
+                TAX = SUBTOTAL * HST;
+                TOTAL = SUBTOTAL + TAX + DELIVERY;
+
+                lblSubTotal.Text = Convert.ToString(Math.Round(SUBTOTAL,2) + "$");
+                lblTax.Text = Convert.ToString(Math.Round(TAX,2) + "$");
+                lblDelivery.Text = "Delivery";
+                lblDeliveryOption.Text = Convert.ToString(DELIVERY + "$");
+                lblTotal.Text = Convert.ToString(Math.Round(TOTAL,2) + "$");
+            }
+            else
+            {
+                lblStateSubTotal.Hide();
+                lblStateTax.Hide();
+                lblDelivery.Hide();
+                lblStateTotal.Hide();
+                lblSubTotal.Hide();
+                lblTax.Hide();
+                lblDeliveryOption.Hide();
+                lblTotal.Hide();
+            }
+        }
+    }
+}
--- PizzaOrder(KuotM)/PizzaOrder(KuotM)/Properties/Resources.Designer.cs
+++ PizzaOrder(KuotM)/PizzaOrder(KuotM)/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PizzaOrder_KuotM_.Properties
-{
-
-
+namespace PizzaOrder_KuotM_.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,242 @@ namespace PizzaOrder_KuotM_.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PizzaOrder_KuotM_.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Bacon {
+            get {
+                object obj = ResourceManager.GetObject("Bacon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bacon_olives_pizza {
+            get {
+                object obj = ResourceManager.GetObject("bacon_olives_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bacon_pizza {
+            get {
+                object obj = ResourceManager.GetObject("bacon_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cheese_pizza {
+            get {
+                object obj = ResourceManager.GetObject("cheese_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap chicken {
+            get {
+                object obj = ResourceManager.GetObject("chicken", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fries {
+            get {
+                object obj = ResourceManager.GetObject("fries", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream FUN {
+            get {
+                return ResourceManager.GetStream("FUN", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Green_olives {
+            get {
+                object obj = ResourceManager.GetObject("Green_olives", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap KrustyKrab {
+            get {
+                object obj = ResourceManager.GetObject("KrustyKrab", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream KrustyKrab1 {
+            get {
+                return ResourceManager.GetStream("KrustyKrab1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap olives_pizza {
+            get {
+                object obj = ResourceManager.GetObject("olives_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Onions {
+            get {
+                object obj = ResourceManager.GetObject("Onions", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Pepperoni {
+            get {
+                object obj = ResourceManager.GetObject("Pepperoni", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pepperoni_bacon_olives_pizza {
+            get {
+                object obj = ResourceManager.GetObject("pepperoni_bacon_olives_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pepperoni_bacon_pizza {
+            get {
+                object obj = ResourceManager.GetObject("pepperoni_bacon_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pepperoni_olives_pizza {
+            get {
+                object obj = ResourceManager.GetObject("pepperoni_olives_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pepperoni_pizza {
+            get {
+                object obj = ResourceManager.GetObject("pepperoni_pizza", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap salad {
+            get {
+                object obj = ResourceManager.GetObject("salad", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SOUND_OFF {
+            get {
+                object obj = ResourceManager.GetObject("SOUND_OFF", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SOUND_ON {
+            get {
+                object obj = ResourceManager.GetObject("SOUND_ON", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
